b7fe1ef1d7b19ff40d4878b39e8c8d9832679768
jvmafra <Mafra@Mafra-PC>
2015-01-31 12:51:52 -0200

    Classe Hotel com a ListaBabas e ListaCarros. A classe precisa ser terminada.

--- Hotel Riviera/src/classes/Hotel.java
+++ Hotel Riviera/src/classes/Hotel.java
@@ -13,23 +13,15 @@ public class Hotel {
 	private List<Contrato> contratos_abertos = new ArrayList();
 	private List<Contrato> contratos_fechados = new ArrayList();
 	private List<Opiniao> opinioes = new ArrayList();
-	private Map<String, Integer> quartos = new HashMap<String, Integer>(7);
+	private ListaCarros carros;
+	private ListaBabas babas;
 	
 	/**
-	 * Ao ser inicializado o hotel, é gerado seu mapa de quartos
+	 * Ao ser inicializado o hotel, sao geradas listas de quartos, carros e babas.
 	 */
 	public Hotel() {
-		geraMapaDeQuartos();	
-	}
-	
-	private void geraMapaDeQuartos(){
-		quartos.put("Presidencial", 5);
-		quartos.put("Luxo Simples", 5);
-		quartos.put("Luxo Duplo", 15);
-		quartos.put("Luxo Triplo", 20);
-		quartos.put("Executivo Simples", 5);
-		quartos.put("Executivo Duplo", 15);
-		quartos.put("Executivo Triplo", 20);
+		carros = new ListaCarros();
+		babas = new ListaBabas();
 	}
 	
 	/**
@@ -67,44 +59,6 @@ public class Hotel {
 	 */
 	public List<Opiniao> getOpinioes() {
 		return opinioes;
-	}
-	
-	/**
-	 * Retorna um Map, que e o mapa de quartos do hotel
-	 */
-	public Map<String, Integer> getQuartos() {
-		return quartos;
-	}
-	
-	/**
-	 * Verifica se um certo quarto esta disponivel
-	 */
-	public boolean isDisponivel(String quarto) throws Exception{
-		if (!(getQuartos().containsKey(quarto)))
-			throw new Exception("Tipo de quarto inexistente");
-		
-		if (getQuartos().get(quarto) == 0)
-			return false;
-		
-		return true;
-			
-	}
-	
-	/**
-	 * Reserva um quarto, decrementando seu valor no mapa
-	 */
-	public void reservaQuarto(String quarto) throws Exception{
-		if (!(getQuartos().containsKey(quarto)))
-			throw new Exception("Tipo de quarto inexistente");
-		
-		if (isDisponivel(quarto)){
-			int nova_quantidade = getQuartos().get(quarto) - 1;
-			getQuartos().remove(quarto);
-			getQuartos().put(quarto, nova_quantidade);
-		}
-		
-		// Precisa ser verificado se existe uma forma melhor de decrementar no mapa sem excluir uma chave
-			
 	}
 	
 	/**
