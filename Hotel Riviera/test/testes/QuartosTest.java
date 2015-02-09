435380e11403d4bf2def57dffde371c1633605b0
adiel <adiel@Dek>
2015-02-08 23:32:19 -0300

    O teste de quartos afe ate q fim

--- Hotel Riviera/test/testes/QuartosTest.java
+++ Hotel Riviera/test/testes/QuartosTest.java
@@ -1,256 +1,220 @@
-//package testes;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//import classes.Quarto;
-//import classes.QuartoExecutivoDuplo;
-//import classes.QuartoExecutivoSimples;
-//import classes.QuartoExecutivoTriplo;
-//import classes.QuartoLuxoDuplo;
-//import classes.QuartoPresidencial;
-//import classes.QuartoLuxoSimples;
-//import classes.QuartoLuxoTriplo;
-//
-//
-//
-//public class QuartosTest {
-//	private Quarto q1;
-//	
-//	@Test
-//	public void testaExecutivoSimples() {
-//		try {
-//			q1 = new QuartoExecutivoSimples(null, 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoSimples("Joao", 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoSimples("Joao Bezerra", 0);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoSimples("Joao Bezerra", -10);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoSimples("Joao Bezerra", 100);
-//		} catch (Exception e) {
-//			Assert.fail();
-//		}
-//
-//	}
-//	
-//	@Test
-//	public void testaExecutivoDuplo() {
-//		try {
-//			q1 = new QuartoExecutivoDuplo(null, 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoDuplo("Joao", 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoDuplo("Joao Bezerra", 0);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoDuplo("Joao Bezerra", -10);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoDuplo("Joao Bezerra", 100);
-//		} catch (Exception e) {
-//			Assert.fail();
-//		}
-//
-//	}
-//	
-//	@Test
-//	public void testaExecutivoTriplo() {
-//		try {
-//			q1 = new QuartoExecutivoTriplo(null, 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoTriplo("Joao", 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoTriplo("Joao Bezerra", 0);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoTriplo("Joao Bezerra", -10);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoExecutivoTriplo("Joao Bezerra", 100);
-//		} catch (Exception e) {
-//			Assert.fail();
-//		}
-//
-//	}
-//	
-//	@Test
-//	public void testaLuxoSimples() {
-//		try {
-//			q1 = new QuartoLuxoSimples(null, 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoSimples("Joao", 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoSimples("Joao Bezerra", 0);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoSimples("Joao Bezerra", -10);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoSimples("Joao Bezerra", 100);
-//		} catch (Exception e) {
-//			Assert.fail();
-//		}
-//
-//	}
-//
-//	@Test
-//	public void testaLuxoDuplo() {
-//		try {
-//			q1 = new QuartoLuxoDuplo(null, 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoDuplo("Joao", 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoDuplo("Joao Bezerra", 0);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoDuplo("Joao Bezerra", -10);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoDuplo("Joao Bezerra", 100);
-//		} catch (Exception e) {
-//			Assert.fail();
-//		}
-//
-//	}
-//	
-//	@Test
-//	public void testaLuxoTriplo() {
-//		try {
-//			q1 = new QuartoLuxoTriplo(null, 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoTriplo("Joao", 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoTriplo("Joao Bezerra", 0);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoTriplo("Joao Bezerra", -10);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoLuxoTriplo("Joao Bezerra", 100);
-//		} catch (Exception e) {
-//			Assert.fail();
-//		}
-//
-//	}
-//	
-//	@Test
-//	public void testaPresidencial() {
-//		try {
-//			q1 = new QuartoPresidencial(null, 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoPresidencial("Joao", 100);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Hospede invalido");
-//		}
-//		try {
-//			q1 = new QuartoPresidencial("Joao Bezerra", 0);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoPresidencial("Joao Bezerra", -10);
-//			Assert.fail();
-//		} catch (Exception e) {
-//			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
-//		}
-//		try {
-//			q1 = new QuartoPresidencial("Joao Bezerra", 100);
-//		} catch (Exception e) {
-//			Assert.fail();
-//		}
-//
-//	}
-//}
+package testes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import classes.Quartos.Quarto;
+import classes.Quartos.QuartoExecutivoDuplo;
+import classes.Quartos.QuartoExecutivoSimples;
+import classes.Quartos.QuartoExecutivoTriplo;
+import classes.Quartos.QuartoLuxoDuplo;
+import classes.Quartos.QuartoLuxoSimples;
+import classes.Quartos.QuartoLuxoTriplo;
+import classes.Quartos.QuartoPresidencial;
+
+
+
+
+public class QuartosTest {
+	private Quarto q1;
+	private Quarto q2;
+	private Quarto q3;
+	
+	@Test
+	public void testaExecutivoSimples() {
+		
+		try {
+			q1 = new QuartoExecutivoSimples(-10);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
+		}
+	}
+	
+	
+	@Test
+	public void testaEqualsExecutivoSimples() throws Exception{
+		q1 = new QuartoExecutivoSimples(10);
+		q2 = new QuartoExecutivoSimples(510);
+		q3 = new QuartoExecutivoSimples(10);
+		
+		Assert.assertFalse(q1.equals(q2));
+		Assert.assertTrue(q1.equals(q3));
+		
+	}
+	
+	@Test
+	public void testaToStringExecutivoSimples() throws Exception{
+		q1 = new QuartoExecutivoSimples(10);
+		Assert.assertEquals(q1.toString(), "N: " + q1.getNumeroDoQuarto() +" - Executivo Simples");
+	}
+	
+	@Test
+	public void testaExecutivoDuplo() {
+		try {
+			q1 = new QuartoExecutivoDuplo(-10);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
+		}
+		}
+
+	@Test
+	public void testaEqualsQuartoExecutivoDuplo() throws Exception{
+		q1 = new QuartoExecutivoDuplo(10);
+		q2 = new QuartoExecutivoDuplo(510);
+		q3 = new QuartoExecutivoDuplo(10);
+		
+		Assert.assertFalse(q1.equals(q2));
+		Assert.assertTrue(q1.equals(q3));
+		
+	}
+	
+	@Test
+	public void testaToStringExecutivoDuplo() throws Exception{
+		q1 = new QuartoExecutivoDuplo(10);
+		Assert.assertEquals(q1.toString(), "N: " + q1.getNumeroDoQuarto() +" - Executivo Duplo");
+	}
+	
+	@Test
+	public void testaExecutivoTriplo() {
+		try {
+			q1 = new QuartoExecutivoTriplo(-10);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
+		}
+		
+
+	}
+	
+	@Test
+	public void testaEqualsQuartoExecutivoTriplo() throws Exception{
+		q1 = new QuartoExecutivoTriplo(10);
+		q2 = new QuartoExecutivoTriplo(510);
+		q3 = new QuartoExecutivoTriplo(10);
+		
+		Assert.assertFalse(q1.equals(q2));
+		Assert.assertTrue(q1.equals(q3));
+		
+	}
+	
+	@Test
+	public void testaToStringExecutivoTriplo() throws Exception{
+		q1 = new QuartoExecutivoTriplo(10);
+		Assert.assertEquals(q1.toString(), "N: " + q1.getNumeroDoQuarto() +" - Executivo Triplo");
+	}
+	
+	@Test
+	public void testaLuxoSimples() {
+		try {
+			q1 = new QuartoLuxoSimples(-10);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
+		}
+		
+	}
+	
+	@Test
+	public void testaEqualsLuxoSimples() throws Exception{
+		q1 = new QuartoLuxoSimples(10);
+		q2 = new QuartoLuxoSimples(510);
+		q3 = new QuartoLuxoSimples(10);
+		
+		Assert.assertFalse(q1.equals(q2));
+		Assert.assertTrue(q1.equals(q3));
+		
+	}
+	
+	@Test
+	public void testaToStringLuxoSimples() throws Exception{
+		q1 = new QuartoLuxoSimples(10);
+		Assert.assertEquals(q1.toString(), "N: " + q1.getNumeroDoQuarto() +" - Luxo Simples");
+	}
+	
+	@Test
+	public void testaLuxoDuplo() {
+		try {
+			q1 = new QuartoLuxoDuplo(-10);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
+		}
+		
+
+	}
+	
+	@Test
+	public void testaEqualsQuartoLuxoDuplos() throws Exception{
+		q1 = new QuartoLuxoDuplo(10);
+		q2 = new QuartoLuxoDuplo(510);
+		q3 = new QuartoLuxoDuplo(10);
+		
+		Assert.assertFalse(q1.equals(q2));
+		Assert.assertTrue(q1.equals(q3));
+		
+	}
+	
+	@Test
+	public void testaToStringLuxoDuplo() throws Exception{
+		q1 = new QuartoLuxoDuplo(10);
+		Assert.assertEquals(q1.toString(), "N: " + q1.getNumeroDoQuarto() +" - Luxo Duplo");
+	}
+	
+	@Test
+	public void testaLuxoTriplo() {
+		try {
+			q1 = new QuartoLuxoTriplo(-10);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
+		}
+		
+	}
+	
+	@Test
+	public void testaEqualsQuartoLuxoTriplo() throws Exception{
+		q1 = new QuartoLuxoTriplo(10);
+		q2 = new QuartoLuxoTriplo(510);
+		q3 = new QuartoLuxoTriplo(10);
+		
+		Assert.assertFalse(q1.equals(q2));
+		Assert.assertTrue(q1.equals(q3));
+		
+	}
+	
+	@Test
+	public void testaToStringLuxoTriplos() throws Exception{
+		q1 = new QuartoLuxoTriplo(10);
+		Assert.assertEquals(q1.toString(), "N: " + q1.getNumeroDoQuarto() +" - Luxo Triplo");
+	}
+	
+	@Test
+	public void testaPresidencial() {
+		try {
+			q1 = new QuartoPresidencial(-10);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Numero do Quarto invalido");
+		}
+		
+	}
+	
+	@Test
+	public void testaEqualsQuartoPresidencial() throws Exception{
+		q1 = new QuartoPresidencial(10);
+		q2 = new QuartoPresidencial(510);
+		q3 = new QuartoPresidencial(10);
+		
+		Assert.assertFalse(q1.equals(q2));
+		Assert.assertTrue(q1.equals(q3));
+		
+	}
+	
+	@Test
+	public void testaToStringPresidencial() throws Exception{
+		q1 = new QuartoPresidencial(10);
+		Assert.assertEquals(q1.toString(), "N: " + q1.getNumeroDoQuarto() +" - Presidencial");
+	}
+}
